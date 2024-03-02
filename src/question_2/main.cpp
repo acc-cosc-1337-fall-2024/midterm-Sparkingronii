34557e1f57afeacd49729966959f64bc49f93ad1
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-03-02 21:25:43 +0000

    Question 2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,40 @@
+#include "question2.h"
+#include<iostream>
+using std::cin; using std::cout;
 
-int main()
+int main() 
 {
+	while (true)
+	{
+		int Option;
+		cout<<"MENU"<<"\n";
+		cout<<"Option 1-Get Sales Commission"<<"\n";
+		cout<<"Option 2-Exit"<<"\n";
 
-    return 0;
+		cout<<"Choose an Option: ";
+		cin>>Option;
+		cout<<Option<<"\n";
+
+		if (Option == 1)
+		{
+	
+			auto Sale = 0;
+            auto Commission_Amount = 0;
+
+			cout<<"Enter a Sales Value: ";
+			cin>>Sale;
+
+			Commission_Amount = get_sales_commission(Sale);
+
+			cout<<Commission_Amount<<"\n";
+
+			//return 0;
+		}
+	
+		
+		else if (Option == 2)
+		{
+			break;
+		}
+	}
 }
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,30 @@
 #include "question2.h"
+#include<iostream>
 
 bool test_config()
 {
     return true;
+}
+
+double get_sales_commission(double sales)
+{
+    int commission = 0;
+      if(sales >= 1500)
+    {
+        commission = sales * .08;
+    }
+    else if(sales >= 1000 && sales <= 1499)
+    {
+        commission = sales * .07;
+    }
+    else if(sales >= 500 && sales <= 999)
+    {
+        commission = sales * .06;
+    }
+    else if(sales >= 0 && sales <= 499)
+    {
+        commission = sales * .05;
+    }
+
+    return commission;
 }
