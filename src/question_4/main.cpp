5ee4d90f2874981f27577950d07265c3e946df88
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-03-05 22:25:06 +0000

    Question 4 edit 2

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -2,7 +2,6 @@
 #include<iostream>
 #include<string>
 using std::string;
-using std::swap;
 using std::cout;
 using std::cin;
 
@@ -11,8 +10,8 @@ int main()
 int Input = 0;
 cout<<"Enter an Inter to Convert to Hexadecimal"<<'\n';
 cin>>Input;
-
-cout<<dec_to_hex (Input);
+string Hexadecimal = dec_to_hex (Input);
+cout<<Hexadecimal<<'\n';
 
     return 0;
 }
